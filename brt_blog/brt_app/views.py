39019cc07edcc7ba611ee24e36b1aeb53cc619e0
dszwarc <dszwarc93@gmail.com>
2023-11-15 14:01:27 -0600

    add Create view for Post model. Create forms.py file for form styling

--- brt_blog/brt_app/views.py
+++ brt_blog/brt_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView
 from .models import Post
 # Create your views here.
 #def home(request):
@@ -11,3 +11,8 @@ class HomeView(ListView):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_detail.html'
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
